c0aa2dba9f6fa4f10473ca10d909e1adf672c7d6
Your Name <you@example.com>
2025-04-05 13:46:14 -0500

    cambio

---
+++ 5 ejercicios22/Areglos.java
@@ -0,0 +1,15 @@
+public class Areglos {
+    public static void main(String[] args) {
+        int[] num =new int [5];
+        String[] vocales = {"a","e","i","o","u"};
+int[] num1={2,3,1,4,5,};
+System.out.println(num1[0]);
+System.out.println(vocales[4]);
+num[0]=3;
+num[1]=10;
+num[2]=5;
+num[3]=6;
+num[4]=9;
+System.out.println(num[2]);
+    }
+}
---
+++ 5 ejercicios22/Imprimirarreglo.java
@@ -0,0 +1,10 @@
+public class Imprimirarreglo {
+    public static void main(String[] args) {
+        String [] alfabeto = {"a","b","c"};
+        int i=0;
+        for(i=0; i<=2;i=i+1){
+            System.out.println(alfabeto[i]);
+        }
+
+    }
+}
