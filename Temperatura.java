7b2d7cb716d72ac5fb502bd0cb1539b5f94949fa
craneal24 <jn0063437@gmail.com>
2025-04-26 22:38:41 -0500

    Ejercicios de entrega objetos

--- Temperatura.java
+++ Temperatura.java
@@ -1,22 +1,10 @@
+package TALLER3;
+
+import java.util.Scanner;
 public class Temperatura {
     public static void main(String[] args) {
-        int Temperatura = 45;
-        if (Temperatura <10){
-            System.out.println("el dia esta muy frio");
-        }
-        if (Temperatura>10){
-            if(Temperatura<=20){
-                System.out.println("dia fresco");
-            }
-        }
-        if (Temperatura>=21){
-            if(Temperatura<=30){
-                System.out.println("dia calido");
-            }
-        }
-        if (Temperatura>30){
-            System.out.println("dia muy caluroso");
-        }
+        Scanner scanner = new Scanner(System.in);
+        
 
     }
 }
